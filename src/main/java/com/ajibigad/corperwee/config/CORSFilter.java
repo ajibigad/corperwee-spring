b7318f9644999a1a803cd72fc05788f339bcbf99
ajibigad <damilola.ajiboye@ymail.com>
2016-03-03 19:47:36 +0100

    altered security config to clear spring cookies in browser

--- src/main/java/com/ajibigad/corperwee/config/CORSFilter.java
+++ src/main/java/com/ajibigad/corperwee/config/CORSFilter.java
@@ -28,7 +28,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "http://localhost");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization,Basic");
+        response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization,Basic,Access-Control-Allow-Origin");
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
         if(!"OPTIONS".equalsIgnoreCase(request.getMethod())) {
